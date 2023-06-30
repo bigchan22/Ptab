4c8bf2838208f1c49258432cb5829825d098b4e2
Byung-Hak Hwang <xoda90@gmail.com>
2023-06-30 17:32:28 +0900

    Add CustomDataset.py

---
+++ CustomDataset.py
@@ -0,0 +1,16 @@
+class CustomDataset(Dataset):
+    def __init__(self, input_data):
+        self.features = input_data.features
+        self.labels = input_data.labels
+        self.rows = input_data.rows
+        self.cols = input_data.columns
+        self.edge_types = input_data.edge_types
+
+    def __len__(self):
+        return len(self.features)
+
+    def __getitem__(self, idx):
+        edge_index = torch.tensor([self.rows[idx], self.cols[idx]], dtype=torch.long)
+        return Data(x=torch.from_numpy(self.features[idx]).float(), edge_index=edge_index, 
+             edge_types = torch.tensor(self.edge_types[idx][:, np.newaxis], dtype=torch.float),
+             y=torch.from_numpy(np.array(self.labels[idx])))
