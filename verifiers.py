a1a7e03d3573e7c5e2e4cf4bd341a234667d815b
Byung-Hak Hwang <xoda90@gmail.com>
2023-09-14 17:47:32 +0900

    Add verify_disconnected_bad_criterion

--- verifiers.py
+++ verifiers.py
@@ -5,26 +5,27 @@ from predictor import *
 
 from itertools import permutations as Perm
 
-def verify_1row(N, MODEL, cutoff=0.7, good_checker=is_good_P_1row_B):
-  chk = True
+def verify_1row(MODEL, cutoff=0.7, good_checker=is_good_P_1row_B):
+  total_cnt = 0
+  incorrect_cnt = 0
+  N = int(MODEL.split('parameters_')[-1][0])
   for P in generate_UIO(N, connected=False):
     for perm in iter_shuffles(cluster_vertices(P)):
       word = list(perm)
       if shape_of_word(P, word) != [N]: continue
+      total_cnt += 1
       pred = predict_tableau(P, word, MODEL)
       if (pred > cutoff and good_checker(P, word)) or (pred < 1-cutoff and not good_checker(P, word)):
         continue
-      chk = False
-      print(f"{P} {word} {pred:.6f} {good_checker(P, word)}")
-  if chk == True:
-    print("Verified!")
-  else:
-    print("Something wrong")
+      incorrect_cnt += 1
+      # print(f"{P} {word} {pred:.6f} {good_checker(P, word)}")
+  return (total_cnt-incorrect_cnt, total_cnt)
 
-def verify_disconnected_2row(N, MODEL, cutoff=0.7, good_checker=is_good_P_1row_B):
-  chk = True
+def verify_disconnected_2row(MODEL, cutoff=0.7, good_checker=is_good_P_1row_B):
+  total_cnt = 0
+  incorrect_cnt = 0
+  N = int(MODEL.split('parameters_')[-1][0])
   for i in range(1, N):
-    print(f"{i}...")
     for P1 in generate_UIO(i, connected=True):
       for P2 in generate_UIO(N-i, connected=True):
         P = [a for a in P1] + [a+i for a in P2]
@@ -48,12 +49,113 @@ def verify_disconnected_2row(N, MODEL, cutoff=0.7, good_checker=is_good_P_1row_B
                 word.append(perm1[j])
             shape = shape_of_word(P, word)
             if shape == None or len(shape) != 2: continue
+            total_cnt += 1
             pred = predict_tableau(P, word, MODEL)
             if (pred > cutoff and good_checker(P, perm1) and good_checker(P, perm2)) or (pred < 1-cutoff and not (good_checker(P, perm1) and good_checker(P, perm2))):
               continue
-            chk = False
-            print(f"{P} {word} {pred:.6f} {good_checker(P, perm1) and good_checker(P, perm2)}")
-  if chk == True:
-    print("Verified!")
-  else:
-    print("Something wrong")
+            incorrect_cnt += 1
+            # print(f"{P} {word} {pred:.6f} {good_checker(P, perm1) and good_checker(P, perm2)}")
+  return (total_cnt-incorrect_cnt, total_cnt)
+
+
+def verify_inclusion_criterion(MODEL, shape_checker=any_shape, gap=0.5, cutoff=0.7):
+    total_cnt = 0
+    incorrect_cnt = 0
+    N = int(MODEL.split('parameters_')[-1][0])
+    for perm in Perm([i+1 for i in range(N)]):
+        word = list(perm)
+        Ps = dict()
+        for P in generate_UIO(N):
+            shape = shape_of_word(P, word)
+            if shape == None: continue
+            if shape_checker(shape) == False: continue
+            pred_prob = predict_tableau(P, word, MODEL)
+            if pred_prob > cutoff: pred = 'GOOD'
+            elif pred_prob < 1 - cutoff: pred = 'BAD'
+            else: pred = 'None'
+            Ps[str(P)] = (pred_prob, pred, shape)
+        for P1 in Ps.keys():
+            for P2 in Ps.keys():
+                if P1 == P2: continue
+                if Ps[P1][2] != Ps[P2][2]: continue
+                if is_included(P1, P2) == False: continue
+                total_cnt += 1
+                if Ps[P1][1] == 'GOOD' and Ps[P2][1] == 'BAD': incorrect_cnt += 1
+    return (total_cnt-incorrect_cnt, total_cnt)
+
+def is_included(P1, P2):
+    for i in range(len(P1)):
+        if P1[i] > P2[i]:
+            return False
+    return True
+
+def verify_disconnected_bad_criterion(N, MODEL, cutoff = 0.7):
+  total_cnt = 0
+  incorrect_cnt = 0
+  N = int(MODEL.split('parameters_')[-1][0])
+  for P in generate_UIO(N, connected=False):
+    if is_connected(P): continue
+    components = split_into_connected_components(P)
+    index = index_set_from_connected_components(components)
+    for perm in iter_shuffles(cluster_vertices(P)):
+      word = list(perm)
+      shape = shape_of_word(P, word)
+      if shape == None: continue
+      conj = conjugate(shape)
+      cnts = [[] for comp in components]
+      k = 0
+      for i in range(len(conj)):
+        for cnt in cnts: cnt.append(0)
+        for j in range(conj[i]):
+          cnts[index[k]][-1] += 1
+          k += 1
+      chk = True
+      for cnt in cnts:
+        if is_non_increasing(cnt) == False:
+          chk = False
+          break
+      if chk == False:
+        total_cnt += 1
+        pred = predict_tableau(P, word, MODEL)
+        if pred >= 1-cutoff:
+          incorrect_cnt += 1
+  return (total_cnt-incorrect_cnt, total_cnt)
+
+def is_connected(P):
+  for i in range(len(P)-1):
+    if P[i] == i+1:
+      return False
+  return True
+
+def split_into_connected_components(P):
+  components = [[]]
+  for i in range(len(P)-1):
+    components[-1].append(i+1)
+    if P[i] == i+1:
+      components.append([])
+  components[-1].append(len(P))
+  return components
+
+def index_set_from_connected_components(components):
+  N = max(max(component) for component in components)
+  index = [-1 for i in range(N+1)]
+  for i, component in enumerate(components):
+    for k in component:
+      index[k] = i
+  return index
+
+def conjugate(lamb):
+  conj = []
+  for i in range(1, lamb[0]+1):
+    cnt = 0
+    for part in lamb:
+      if part >= i:
+        cnt += 1
+    conj.append(cnt)
+  return conj
+
+def is_non_increasing(seq):
+  for i in range(1, len(seq)):
+    if seq[i-1] < seq[i]:
+      return False
+  return True
